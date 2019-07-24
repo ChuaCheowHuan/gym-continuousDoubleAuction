f5d617b0957e8abfd747cdbf3f0cbe7c93329e12
Chua Cheow Huan <17569306+ChuaCheowHuan@users.noreply.github.com>
2019-07-24 18:05:33 +0800

    total_sys_nav bug, most likely caused by size decrease and/or covered cases

--- MM_env/exchg/trader.py
+++ MM_env/exchg/trader.py
@@ -33,22 +33,7 @@ class Trader(Random_agent):
         else:
             order = {}
         return order
-    """
-    # pass action to step
-    def select_random_action(self, exchg):
-        #type = np.random.randint(0, 1, size=1) # type, draw 1 int, 0(market) to 1(limit)
-        type = random.choice(['market','limit'])
-        #type = random.choice(['limit'])
-        #side = np.random.randint(-1, 1, size=1) # side, draw 1 int, -1(ask), 0(None), 1(bid)
-        side = random.choice(['bid',None,'ask'])
-        size = random.randrange(1, 100, 100) # size in 100s from 0(min) to 1000(max)
-        price = random.randrange(1, 10, 1) # price from 1(min) to 100(max)
-        action = {"type": type,
-                  "side": side,
-                  "size": size,
-                  "price": price}
-        return action
-    """
+
     def process_counter_party(self, agents, trade):
         for counter_party in agents: # search for counter_party
             if counter_party.ID == trade.get('counter_party').get('ID'):
