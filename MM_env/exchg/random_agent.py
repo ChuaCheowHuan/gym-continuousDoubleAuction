5dc26e6f27b2423b13bef4ce6cfe373fc139f611
Chua Cheow Huan <17569306+ChuaCheowHuan@users.noreply.github.com>
2019-07-27 21:25:25 +0800

    added comments in random agent

--- MM_env/exchg/random_agent.py
+++ MM_env/exchg/random_agent.py
@@ -30,6 +30,16 @@ norm_dist = tf.distributions.Normal(loc=mu, scale=sigma)
 
 
 
+hidden_layer = tf.layers.dense(self.s, num_hidden, tf.nn.relu, kernel_initializer = a_w, name='a_hidden', trainable=trainable)
+logits = tf.layers.dense(hidden_layer, A_DIM, tf.nn.softmax, kernel_initializer = a_w, name='mu', trainable=trainable)
+# tanh [-1,1]
+mu = tf.layers.dense(hidden_layer, A_DIM, tf.nn.tanh, kernel_initializer = a_w, name='mu', trainable=trainable)
+# softplus [0,inf)
+sigma = tf.layers.dense(hidden_layer, A_DIM, tf.nn.softplus, kernel_initializer = a_w, name='sigma', trainable=trainable) + 1e-4
+norm_dist = tf.distributions.Normal(loc=mu, scale=sigma)
+
+
+
 # Let mean vector and co-variance be:
 mu = [1., 2] # number of variables is 2 since mu is 1x2
 cov = [[ 1,  3/5],[ 3/5,  2]] # cov is 2x2 since number of variables is 2
