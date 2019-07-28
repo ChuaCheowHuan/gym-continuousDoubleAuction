82f2af6d47d2465648e1ba84e84658ce932f17d5
Chua Cheow Huan <17569306+ChuaCheowHuan@users.noreply.github.com>
2019-07-28 23:50:39 +0800

    edit multiagent_MM_env

--- MM_env/ray_rllib_multi_agent/multiagent_MM_env.py
+++ MM_env/ray_rllib_multi_agent/multiagent_MM_env.py
@@ -39,32 +39,24 @@ class CustomModel1(Model):
         # by entering it explicitly with tf.AUTO_REUSE. This creates the
         # variables for the 'fc1' layer in a global scope called 'shared'
         # outside of the policy's normal variable scope.
-        with tf.variable_scope(
-                tf.VariableScope(tf.AUTO_REUSE, "shared"),
-                reuse=tf.AUTO_REUSE,
-                auxiliary_name_scope=False):
-            last_layer = tf.layers.dense(
-                input_dict["obs"], 64, activation=tf.nn.relu, name="fc1")
-        last_layer = tf.layers.dense(
-            last_layer, 64, activation=tf.nn.relu, name="fc2")
-        output = tf.layers.dense(
-            last_layer, num_outputs, activation=None, name="fc_out")
+        with tf.variable_scope(tf.VariableScope(tf.AUTO_REUSE, "shared"),
+                               reuse=tf.AUTO_REUSE,
+                               auxiliary_name_scope=False):
+            last_layer = tf.layers.dense(input_dict["obs"], 64, activation=tf.nn.relu, name="fc1")
+        last_layer = tf.layers.dense(last_layer, 64, activation=tf.nn.relu, name="fc2")
+        output = tf.layers.dense(last_layer, num_outputs, activation=None, name="fc_out")
         return output, last_layer
 
 
 class CustomModel2(Model):
     def _build_layers_v2(self, input_dict, num_outputs, options):
         # Weights shared with CustomModel1
-        with tf.variable_scope(
-                tf.VariableScope(tf.AUTO_REUSE, "shared"),
-                reuse=tf.AUTO_REUSE,
-                auxiliary_name_scope=False):
-            last_layer = tf.layers.dense(
-                input_dict["obs"], 64, activation=tf.nn.relu, name="fc1")
-        last_layer = tf.layers.dense(
-            last_layer, 64, activation=tf.nn.relu, name="fc2")
-        output = tf.layers.dense(
-            last_layer, num_outputs, activation=None, name="fc_out")
+        with tf.variable_scope(tf.VariableScope(tf.AUTO_REUSE, "shared"),
+                               reuse=tf.AUTO_REUSE,
+                               auxiliary_name_scope=False):
+            last_layer = tf.layers.dense(input_dict["obs"], 64, activation=tf.nn.relu, name="fc1")
+        last_layer = tf.layers.dense(last_layer, 64, activation=tf.nn.relu, name="fc2")
+        output = tf.layers.dense(last_layer, num_outputs, activation=None, name="fc_out")
         return output, last_layer
 
 
