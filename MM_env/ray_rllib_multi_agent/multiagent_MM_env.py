87c2b74576d6e0c7425f5bb80596384ccbc5d860
Chua Cheow Huan <17569306+ChuaCheowHuan@users.noreply.github.com>
2019-07-30 02:52:54 +0800

    set state, may need parametric action spaces

--- MM_env/ray_rllib_multi_agent/multiagent_MM_env.py
+++ MM_env/ray_rllib_multi_agent/multiagent_MM_env.py
@@ -22,6 +22,18 @@ from ray.rllib.tests.test_multi_agent_env import MultiCartpole
 from ray.tune.registry import register_env
 from ray.rllib.utils import try_import_tf
 
+
+
+import sys
+
+if "../" not in sys.path:
+    sys.path.append("../")
+
+#from exchg.x.y import z
+from exchg.exchg import Exchg
+
+
+
 tf = try_import_tf()
 
 parser = argparse.ArgumentParser()
@@ -31,7 +43,7 @@ parser.add_argument("--num-policies", type=int, default=2)
 parser.add_argument("--num-iters", type=int, default=20)
 parser.add_argument("--simple", action="store_true")
 
-
+"""
 class CustomModel1(Model):
     def _build_layers_v2(self, input_dict, num_outputs, options):
         # Example of (optional) weight sharing between two different policies.
@@ -58,7 +70,36 @@ class CustomModel2(Model):
         last_layer = tf.layers.dense(last_layer, 64, activation=tf.nn.relu, name="fc2")
         output = tf.layers.dense(last_layer, num_outputs, activation=None, name="fc_out")
         return output, last_layer
+"""
+class CustomModel1(Model):
+    def _build_layers_v2(self, input_dict, num_outputs, options):
+        # Example of (optional) weight sharing between two different policies.
+        # Here, we share the variables defined in the 'shared' variable scope
+        # by entering it explicitly with tf.AUTO_REUSE. This creates the
+        # variables for the 'fc1' layer in a global scope called 'shared'
+        # outside of the policy's normal variable scope.
+        with tf.variable_scope(tf.VariableScope(tf.AUTO_REUSE, "shared"),
+                               reuse=tf.AUTO_REUSE,
+                               auxiliary_name_scope=False):
+            last_layer = tf.layers.dense(input_dict["obs"], 64, activation=tf.nn.relu, name="fc1")
+        last_layer = tf.layers.dense(last_layer, 64, activation=tf.nn.relu, name="fc2")
+        output = tf.layers.dense(last_layer, num_outputs, activation=None, name="fc_out")
 
+
+
+        return output, last_layer
+
+
+class CustomModel2(Model):
+    def _build_layers_v2(self, input_dict, num_outputs, options):
+        # Weights shared with CustomModel1
+        with tf.variable_scope(tf.VariableScope(tf.AUTO_REUSE, "shared"),
+                               reuse=tf.AUTO_REUSE,
+                               auxiliary_name_scope=False):
+            last_layer = tf.layers.dense(input_dict["obs"], 64, activation=tf.nn.relu, name="fc1")
+        last_layer = tf.layers.dense(last_layer, 64, activation=tf.nn.relu, name="fc2")
+        output = tf.layers.dense(last_layer, num_outputs, activation=None, name="fc_out")
+        return output, last_layer
 
 if __name__ == "__main__":
     args = parser.parse_args()
