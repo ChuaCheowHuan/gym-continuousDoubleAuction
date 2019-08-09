67f3df377d63e7d3fe93c83b139809c6a0a46cf9
Chua Cheow Huan <17569306+ChuaCheowHuan@users.noreply.github.com>
2019-08-10 02:37:03 +0800

    package as a gym env & installable via pip, added seed (for gym action space) to random policy

---
+++ gym-continuousDoubleAuction/gym_continuousDoubleAuction/envs/agent/random_agent.py
@@ -0,0 +1,142 @@
+import random
+
+class Random_agent(object):
+
+    # pass action to step
+    def select_random_action(self):
+        type_side = random.randrange(0, 5, 1) # type_side: None=0, market_bid=1, market_ask=2, limit_bid=3, limit_ask=4
+        size = random.randrange(1, 100, 1) # size in 100s from 0(min) to 1000(max)
+        price = random.randrange(1, 10, 1) # price from 1(min) to 100(max)
+        act = (type_side, size, price)
+        print('select_random_action act:', act)
+        return act
+
+"""
+hidden_layer = tf.layers.dense(self.s, num_hidden, tf.nn.relu, kernel_initializer = a_w, name='a_hidden', trainable=trainable)
+# tanh [-1,1]
+mu = tf.layers.dense(hidden_layer, A_DIM, tf.nn.tanh, kernel_initializer = a_w, name='mu', trainable=trainable)
+# softplus {0,inf)
+sigma = tf.layers.dense(hidden_layer, A_DIM, tf.nn.softplus, kernel_initializer = a_w, name='sigma', trainable=trainable) + 1e-4
+norm_dist = tf.distributions.Normal(loc=mu, scale=sigma)
+
+
+
+hidden_layer = tf.layers.dense(self.s, num_hidden, tf.nn.relu, kernel_initializer = a_w, name='a_hidden', trainable=trainable)
+logits = tf.layers.dense(hidden_layer, A_DIM, tf.nn.softmax, kernel_initializer = a_w, name='mu', trainable=trainable)
+# tanh [-1,1]
+mu = tf.layers.dense(hidden_layer, A_DIM, tf.nn.tanh, kernel_initializer = a_w, name='mu', trainable=trainable)
+# softplus [0,inf)
+sigma = tf.layers.dense(hidden_layer, A_DIM, tf.nn.softplus, kernel_initializer = a_w, name='sigma', trainable=trainable) + 1e-4
+norm_dist = tf.distributions.Normal(loc=mu, scale=sigma)
+
+
+
+#https://www.tensorflow.org/api_docs/python/tf/contrib/distributions/MultivariateNormalFullCovariance
+import tensorflow_probability as tfp
+tfd = tfp.distributions
+
+# Initialize a single 3-variate Gaussian.
+mu = [1., 2, 3]
+cov = [[ 0.36,  0.12,  0.06],
+       [ 0.12,  0.29, -0.13],
+       [ 0.06, -0.13,  0.26]]
+mvn = tfd.MultivariateNormalFullCovariance(
+    loc=mu,
+    covariance_matrix=cov)
+
+mvn.mean().eval()
+# ==> [1., 2, 3]
+
+# Covariance agrees with covariance_matrix.
+mvn.covariance().eval()
+# ==> [[ 0.36,  0.12,  0.06],
+#      [ 0.12,  0.29, -0.13],
+#      [ 0.06, -0.13,  0.26]]
+
+# Compute the pdf of an observation in `R^3` ; return a scalar.
+mvn.prob([-1., 0, 1]).eval()  # shape: []
+
+# Initialize a 2-batch of 3-variate Gaussians.
+mu = [[1., 2, 3],
+      [11, 22, 33]]              # shape: [2, 3]
+covariance_matrix = ...  # shape: [2, 3, 3], symmetric, positive definite.
+mvn = tfd.MultivariateNormalFullCovariance(
+    loc=mu,
+    covariance=covariance_matrix)
+
+# Compute the pdf of two `R^3` observations; return a length-2 vector.
+x = [[-0.9, 0, 0.1],
+     [-10, 0, 9]]     # shape: [2, 3]
+mvn.prob(x).eval()    # shape: [2]
+
+
+
+# Let mean vector and co-variance be:
+mu = [1., 2] # number of variables is 2 since mu is 1x2
+cov = [[ 1,  3/5],[ 3/5,  2]] # cov is 2x2 since number of variables is 2
+
+#Multivariate Normal distribution
+gaussian = tf.contrib.distributions.MultivariateNormalFullCovariance(
+           loc=mu,
+           covariance_matrix=cov)
+
+# Generate a mesh grid to plot the distributions
+X, Y = tf.meshgrid(tf.range(-3, 3, 0.1), tf.range(-3, 3, 0.1))
+idx = tf.concat([tf.reshape(X, [-1, 1]), tf.reshape(Y,[-1,1])], axis =1)
+prob = tf.reshape(gaussian.prob(idx), tf.shape(X))
+
+with tf.Session() as sess:
+   p = sess.run(prob)
+   m, c = sess.run([gaussian.mean(), gaussian.covariance()])
+   # m is [1., 2.]
+   # c is [[1, 0.6], [0.6, 2]]
+
+
+
+tfd = tfp.distributions
+
+# Initialize a single 2-variate Gaussian.
+mvn = tfd.MultivariateNormalDiag(
+    loc=[1., -1],
+    scale_diag=[1, 2.])
+
+mvn.mean().eval()
+# ==> [1., -1]
+
+mvn.stddev().eval()
+# ==> [1., 2]
+
+# Evaluate this on an observation in `R^2`, returning a scalar.
+mvn.prob([-1., 0]).eval()  # shape: []
+
+# Initialize a 3-batch, 2-variate scaled-identity Gaussian.
+mvn = tfd.MultivariateNormalDiag(
+    loc=[1., -1],
+    scale_identity_multiplier=[1, 2., 3])
+
+mvn.mean().eval()  # shape: [3, 2]
+# ==> [[1., -1]
+#      [1, -1],
+#      [1, -1]]
+
+mvn.stddev().eval()  # shape: [3, 2]
+# ==> [[1., 1],
+#      [2, 2],
+#      [3, 3]]
+
+# Evaluate this on an observation in `R^2`, returning a length-3 vector.
+mvn.prob([-1., 0]).eval()  # shape: [3]
+
+# Initialize a 2-batch of 3-variate Gaussians.
+mvn = tfd.MultivariateNormalDiag(
+    loc=[[1., 2, 3],
+         [11, 22, 33]]           # shape: [2, 3]
+    scale_diag=[[1., 2, 3],
+                [0.5, 1, 1.5]])  # shape: [2, 3]
+
+# Evaluate this on a two observations, each in `R^3`, returning a length-2
+# vector.
+x = [[-1., 0, 1],
+     [-11, 0, 11.]]   # shape: [2, 3].
+mvn.prob(x).eval()    # shape: [2]
+"""
---
+++ gym-continuousDoubleAuction/gym_continuousDoubleAuction/envs/continuousDoubleAuction_env.py
@@ -0,0 +1,122 @@
+#import tensorflow as tf
+
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+import ray
+from ray.rllib.env.multi_agent_env import MultiAgentEnv
+
+from .orderbook.orderbook import OrderBook
+from .exchg.exchg_helper import Exchg_Helper
+from .agent.trader import Trader
+
+# The exchange environment
+class continuousDoubleAuctionEnv(gym.Env, Exchg_Helper, MultiAgentEnv):
+
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self, num_of_agents=2, init_cash=0, tick_size=1, tape_display_length=10, max_step=100):
+        super(continuousDoubleAuctionEnv, self).__init__(init_cash, tick_size, tape_display_length)
+
+        self.next_states = {}
+        self.rewards = {}
+        self.dones = {}
+        self.done_set = set()
+        self.infos = {}
+
+        # step when actions by all traders are executed, not tick time
+        # within a step, multiple trades(ticks) could happened
+        self.t_step = 0
+        self.max_step = max_step
+
+        # list of agents or traders
+        self.agents = [Trader(ID, init_cash) for ID in range(0, num_of_agents)]
+
+        # observation space per agent:
+        # array([[ 1.,  0., -1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+        #        [-1., -4., -4.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+        #        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+        #        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.]])
+        inf = float('inf')
+        neg_inf = float('-inf')
+        obs_row = 4
+        obs_col = 10
+        self.observation_space = spaces.Box(low=neg_inf, high=inf, shape=(obs_row,obs_col))
+
+        # ********** NEED TO PREPROCESS ACTION FROM NN BEFORE EXECUTION **********
+        # NEED TO DECODE type_side
+        # NEED TO ADD trader.ID before randomizing execution sequence
+        # order per agent: {'ID': 0, 'type': 'market', 'side': 'bid', 'size': 1, 'price': 8}
+        # action space per agent: {'type_side': 0-4, 'size': 1-inf, 'price': tick_size-inf}
+        self.action_space = self.disc_act()
+        #self.action_space = self.cont_act()
+
+    # reset
+    def reset(self):
+
+        self.LOB = OrderBook(1, self.tape_display_length) # new limit order book
+        #self.LOB = OrderBook(0.25, self.tape_display_length) # new limit order book
+        self.agg_LOB = {}
+        self.agg_LOB_aft = {}
+
+        self.next_states = {}
+        self.rewards = {}
+        self.dones = {}
+        self.done_set = set()
+        self.infos = {}
+
+        self.trades = {} # a trade between init_party & counter_party
+        self.order_in_book = {} # unfilled orders goes to LOB
+
+        self.t_step = 0
+
+        self.reset_traders_acc()
+
+        return self.reset_traders_agg_LOB()
+
+    # actions is a list of actions from all agents (traders) at t step
+    # each action is a list of (ID, type, side, size, price)
+    def step(self, actions):
+
+        print('step actions:', actions)
+# clip & floor div before passing into LOB
+# step actions: {0: array([ 1.1145465,  2.0979862, -1.5236733], dtype=float32), 1: array([-0.5973312 ,  4.129451  , -0.29671174], dtype=float32)}
+# (pid=9574) actions: [{'ID': 0, 'type': 'limit', 'side': 'ask', 'size': 2.0979862, 'price': -1.5236733}, {'ID': 1, 'type': 'limit', 'side': 'ask', 'size': 4.129451, 'price': -0.29671174}]
+# (pid=9574) shuffle_actions: [{'ID': 1, 'type': 'limit', 'side': 'ask', 'size': 4.129451, 'price': -0.29671174}, {'ID': 0, 'type': 'limit', 'side': 'ask', 'size': 2.0979862, 'price': -1.5236733}]
+
+        self.next_states, self.rewards, self.dones, self.infos = {}, {}, {}, {}
+        self.agg_LOB = self.set_agg_LOB() # LOB state at t before processing LOB
+        actions = self.set_actions(actions) # format actions from nn output to be acceptable by LOB
+        actions = self.rand_exec_seq(actions, 0) # randomized traders execution sequence
+        self.do_actions(actions) # Begin processing LOB
+        self.mark_to_mkt() # mark to market
+        # after processing LOB
+        state_input = self.prep_next_state()
+        self.next_states, self.rewards, self.dones, self.infos = self.set_step_outputs(state_input)
+        self.t_step += 1
+
+        self.render()
+
+        return self.next_states, self.rewards, self.dones, self.infos
+
+    # render
+    def render(self):
+
+        print('\n********** render **********:\n')
+
+        print('\nt_step:\n', self.t_step)
+        print('\nLOB:\n', self.LOB)
+        print('\nagg_LOB:\n', self.agg_LOB)
+        print('\nagg_LOB_aft:\n', self.agg_LOB_aft)
+        print('\nnext_states:\n', self.next_states)
+        print('\nrewards:\n', self.rewards)
+        print('\ndones:\n', self.dones)
+        print('\ninfos:\n', self.infos)
+        self.print_accs()
+        print('total_sys_profit:', self.total_sys_profit())
+        print('total_sys_nav:', self.total_sys_nav())
+        #return 0
+
+    def close(self):
+        pass
