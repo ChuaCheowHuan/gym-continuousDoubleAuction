1f51da73af1d43ccd7dd1242b306eae3cc428bc7
Chua Cheow Huan <17569306+ChuaCheowHuan@users.noreply.github.com>
2020-03-16 21:35:20 +0800

    Add comments to exchg_helper.py, info_helper.py, reward_helper.py, state_helper.py.

--- gym_continuousDoubleAuction/envs/exchg/reward_helper.py
+++ gym_continuousDoubleAuction/envs/exchg/reward_helper.py
@@ -3,8 +3,12 @@ import pandas as pd
 
 class Reward_Helper(object):
 
-    # reward per t step
-    # reward = nav@t+1 - nav@t
     def set_reward(self, rewards, trader):
+        """
+        reward per t step
+        reward = nav@t+1 - nav@t
+        """
+
         rewards[trader.ID] = float(trader.acc.nav - trader.acc.prev_nav)
+
         return rewards
--- gym_continuousDoubleAuction/envs/exchg/state_helper.py
+++ gym_continuousDoubleAuction/envs/exchg/state_helper.py
@@ -4,15 +4,26 @@ class State_Helper(object):
 
     # reset traders LOB observations/states
     def reset_traders_agg_LOB(self):
+        """
+        Set observation state for all traders.
+
+        https://github.com/ray-project/ray/blob/master/doc/source/rllib-env.rst
+        May not need to have all traders(agents) in this state(obs) dict.
+        """
+
         states = {}
         for trader in self.agents:
-
-            # https://github.com/ray-project/ray/blob/master/doc/source/rllib-env.rst
-            # May not need to have all traders(agents) in this state(obs) dict.
             states[trader.ID] = self.set_agg_LOB()
+
         return states
 
     def prep_next_state(self):
+        """
+        Return:
+            state_diff: The state of the aggregated LOB  after all actions are
+                        executed.
+        """
+
         self.agg_LOB_aft = self.set_agg_LOB() # LOB state at t+1 after processing LOB
 
         # ********** state_diff should be used in obs preprocessing, not here **********
@@ -22,20 +33,42 @@ class State_Helper(object):
         return state_diff
 
     def set_next_state(self, next_states, trader, state_input):
+        """
+        Set next state.
+
+        Argument:
+            next_states: Dictionary.
+            trader: A trader object.
+            state_input: The state of the aggregated LOB  after all actions are
+                         executed
+
+        Returns:
+            next_states: Dictionary of states for each trader.
+        """
+
         next_states[trader.ID] = state_input
+
         return next_states
 
-    # price_map is an OrderTree object (SortedDict object)
-    # SortedDict object has key & value
-    # key is price, value is an OrderList object
     def set_agg_LOB(self):
+        """
+        Set the aggregated LOB.
+
+        Return: list of np.arrays
+
+        Notes:
+            price_map is an OrderTree object (SortedDict object).
+            SortedDict object has key & value, key is price, value is an
+            OrderList object.
+        """
+
         k_rows = 10
         bid_price_list = np.zeros(k_rows)
         bid_size_list = np.zeros(k_rows)
         ask_price_list = np.zeros(k_rows)
         ask_size_list = np.zeros(k_rows)
 
-        # LOB
+        # LOB bids
         if self.LOB.bids != None and len(self.LOB.bids) > 0:
             # reversed because we want the highest bid as the first entry in the np.array
             for k, set in enumerate(reversed(self.LOB.bids.price_map.items())):
@@ -44,7 +77,7 @@ class State_Helper(object):
                     bid_size_list[k] = set[1].volume # set[1] is an OrderList object (value) & volume is total volume of the OrderList object
                 else:
                     break
-
+        # LOB asks
         if self.LOB.asks != None and len(self.LOB.asks) > 0:
             # lowest ask is the first entry in the np.array
             for k, set in enumerate(self.LOB.asks.price_map.items()):
@@ -64,10 +97,19 @@ class State_Helper(object):
                     break
         return [bid_size_list, bid_price_list, ask_size_list, ask_price_list] # list of np.arrays
 
-    # state_diff should be used in obs preprocessing if needed
     def state_diff(self, agg_LOB, agg_LOB_aft):
+        """
+        Argument:
+            agg_LOB: Aggregated LOB at time step t.
+            agg_LOB_aft: Aggregated LOB at time step t+1.
+
+        Returns:
+            state_diff: The difference between agg_LOB_aft & agg_LOB.
+
+        Notes:
+            state_diff should be used in obs preprocessing if needed
+        """
         state_diff = []
-        #state_diff = np.array()
         for (state_row, next_state_row) in zip(agg_LOB, agg_LOB_aft):
             diff = next_state_row - state_row
             list_diff = list(diff)
